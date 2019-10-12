6c07cca9660b6d0a90ecb0edae0bd810cefd5acf
Jessika Murphy <Jessika.Murphy@temple.edu>
2019-10-12 14:54:57 -0400

    first commit

---
+++ ui/src/app/auth-guard.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core'
+import { AuthService } from './auth.service'
+import { CanActivate, Router } from '@angular/router'
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    }
+
+    this.auth.login()
+  }
+}
---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  gateway: '',
+  callback: 'http://localhost:4200/callback',
+  domain: 'dev--gjcrbou.auth0.com',
+  clientId: 'gpOFaOOUxm36Di7bVsCuSod89Yl4XzZV',
+  audience: 'https://kanji-connection-api/'
+};
